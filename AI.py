403b640a964ef3362d1648086c4b712adf222b78
David Chung <chungs_kr@hotmail.com>
2015-03-02 20:42:06 -0800

    NB Classifier modified

--- AI.py
+++ AI.py
@@ -1,28 +1,35 @@
 import nltk
 from nltk import classify
+import Utils
 
 
 class NaiveBayesClassifier:
 
-    def __init__(self, tweetCollection,features):
-        self.features = features
+    def __init__(self, tweetCollection):
         self.test_data = tweetCollection
-        self.nbClassifier = nltk.NaiveBayesClassifier.train(FeatureExtractor(tweetCollection,features).get_feature_set_NB())
+        self.nbClassifier = nltk.NaiveBayesClassifier.train(FeatureExtractor(tweetCollection).get_feature_set_NB())
 
+    def classifty(self,tweetCollection):
+        features = FeatureExtractor(tweetCollection).get_feature_set_NB()
+        result = []
+        for oneFeature in features:
+            result.append(self.nbClassifier.classify(oneFeature))
+        return result
 
     def accuracy_test(self,tweetCollection):
         # Compute Accuracy of classifier
-        accuracy = classify.accuracy(self.nbClassifier, FeatureExtractor(tweetCollection,self.features).get_feature_set_NB())
+        accuracy = classify.accuracy(self.nbClassifier, FeatureExtractor(tweetCollection).get_feature_set_NB())
         # Print K most informative features
         print(self.nbClassifier.show_most_informative_features(10))
         return accuracy*100
 
 class FeatureExtractor:
-    def __init__(self,tweetCollection,features):
-        self.features = features
+    def __init__(self,tweetCollection):
         self.tweetCollection = tweetCollection
 
-    def get_feature_NB(self, tweet):
+    def get_feature_most_common(self, tweet, count):
+        fdist = Utils.get_frequency_distribution(self.tweetCollection.generate_nltk_text(1))
+        features = [feature[0] for feature in fdist.most_common(20)]
         featureset ={}
         tweetTokens = tweet.get_tweet_tokens()
         for feature in self.features:
